fa67c1c16c62df3c9be0f0bd46638be48caa70cd
mantkowicz <antek15@gmail.com>
2015-04-20 03:04:18 +0200

    pobieranie plików

--- src/com/mantkowicz/tg/screens/SplashScreen.java
+++ src/com/mantkowicz/tg/screens/SplashScreen.java
@@ -1,15 +1,24 @@
 package com.mantkowicz.tg.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Net;
+import com.badlogic.gdx.Net.HttpRequest;
+import com.badlogic.gdx.Net.HttpResponse;
+import com.badlogic.gdx.Net.HttpResponseListener;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.mantkowicz.tg.enums.ActionType;
+import com.mantkowicz.tg.logger.Logger;
 import com.mantkowicz.tg.main.Main;
 import com.mantkowicz.tg.managers.ActionManager;
 
-public class SplashScreen extends BaseScreen
+public class SplashScreen extends BaseScreen implements HttpResponseListener
 {
 	private Image splashImage;
 	private Label label;
+	
+	HttpRequest hr;
 	
 	public SplashScreen(Main game)
 	{
@@ -29,11 +38,18 @@ public class SplashScreen extends BaseScreen
 		this.label.getColor().a = 0;
 		this.setCenter(this.label, -150);
 		
-		this.nextScreen = new MenuScreen( this.game );
+		this.nextScreen = new GameScreen( this.game );
+		//this.nextScreen = new MenuScreen( this.game );
 		this.stage.addListener(this.nextScreenListener);
 		
 		this.stage.addActor(this.splashImage);
 		this.stage.addActor(this.label);
+		
+		hr = new HttpRequest();
+		hr.setUrl("http://www.mantkowicz.pl/tp/font.ttf");
+		hr.setMethod(Net.HttpMethods.GET);
+		hr.setContent("");
+		Gdx.net.sendHttpRequest(hr, this);
 	}
 
 	@Override
@@ -50,5 +66,32 @@ public class SplashScreen extends BaseScreen
 				this.label.addAction( ActionManager.getInstance().getAction(ActionType.HIDE) );
 			}
 		}
+	}
+
+	@Override
+	public void handleHttpResponse(HttpResponse httpResponse) {
+
+		final int statusCode = httpResponse.getStatus().getStatusCode();
+		
+		FileHandle fh = Gdx.files.local("font.ttf");
+		
+		byte[] fileBytes = httpResponse.getResult();
+		
+		fh.writeBytes(fileBytes, false);
+		
+		Logger.log(this, statusCode);
+		
+	}
+
+	@Override
+	public void failed(Throwable t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cancelled() {
+		// TODO Auto-generated method stub
+		
 	}
 }
